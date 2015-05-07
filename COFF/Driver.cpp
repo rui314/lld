4c825275aed68b655b488415266f3e57c4cde994
Rui Ueyama <ruiu@google.com>
2015-05-07 11:02:55 -0700

    Warn on unknown command line options.

--- COFF/Driver.cpp
+++ COFF/Driver.cpp
@@ -7,9 +7,77 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/Twine.h"
+#include "llvm/Object/COFF.h"
+#include "llvm/Option/Arg.h"
+#include "llvm/Option/ArgList.h"
+#include "llvm/Option/Option.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
+#include <memory>
+#include <sstream>
+
+using llvm::Twine;
+
+// Create enum with OPT_xxx values for each option in Options.td
+enum {
+  OPT_INVALID = 0,
+#define OPTION(PREFIX, NAME, ID, KIND, GROUP, ALIAS, ALIASARGS, FLAGS, PARAM, \
+               HELP, META) \
+          OPT_##ID,
+#include "Options.inc"
+#undef OPTION
+};
+
+// Create prefix string literals used in Options.td
+#define PREFIX(NAME, VALUE) const char *const NAME[] = VALUE;
+#include "Options.inc"
+#undef PREFIX
+
+// Create table mapping all options defined in Options.td
+static const llvm::opt::OptTable::Info infoTable[] = {
+#define OPTION(PREFIX, NAME, ID, KIND, GROUP, ALIAS, ALIASARGS, FLAGS, PARAM, \
+               HELPTEXT, METAVAR)   \
+  { PREFIX, NAME, HELPTEXT, METAVAR, OPT_##ID, llvm::opt::Option::KIND##Class, \
+    PARAM, FLAGS, OPT_##GROUP, OPT_##ALIAS, ALIASARGS },
+#include "Options.inc"
+#undef OPTION
+};
+
+namespace {
+class COFFOptTable : public llvm::opt::OptTable {
+public:
+  COFFOptTable()
+    : OptTable(infoTable, llvm::array_lengthof(infoTable),
+	       /* ignoreCase */ true) {}
+};
+} // anonymous namespace
+
 namespace lld {
 
 bool linkCOFF(int argc, const char *argv[]) {
+  std::unique_ptr<llvm::opt::InputArgList> parsedArgs;
+  COFFOptTable table;
+  unsigned missingIndex;
+  unsigned missingCount;
+  parsedArgs.reset(table.ParseArgs(&argv[1], &argv[argc],
+                                   missingIndex, missingCount));
+  if (missingCount) {
+    llvm::errs() << "error: missing arg value for '"
+		 << parsedArgs->getArgString(missingIndex) << "' expected "
+		 << missingCount << " argument(s).\n";
+    return false;
+  }
+
+  for (auto *arg : parsedArgs->filtered(OPT_UNKNOWN)) {
+    llvm::errs() << "warning: ignoring unknown argument: "
+		 << arg->getSpelling() << "\n";
+  }
+
+  for (auto *arg : parsedArgs->filtered(OPT_INPUT))
+    llvm::dbgs() << arg->getValue() << "\n";
   return true;
 }
 
