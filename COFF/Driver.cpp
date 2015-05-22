59ad1400db522fab83048052571c596674a2fa64
Rui Ueyama <ruiu@google.com>
2015-05-22 13:58:02 -0700

    Add CommandLine.{cpp,h}.

--- COFF/Driver.cpp
+++ COFF/Driver.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "Allocator.h"
+#include "CommandLine.h"
 #include "Config.h"
 #include "Reader.h"
 #include "SymbolTable.h"
@@ -102,27 +103,6 @@ static std::vector<StringRef> splitPathList(StringRef str) {
     ret.push_back(path);
   }
   return ret;
-}
-
-ErrorOr<std::unique_ptr<llvm::opt::InputArgList>>
-parseArgs(int Argc, const char *Argv[]) {
-  COFFOptTable Table;
-  unsigned MissingIndex;
-  unsigned MissingCount;
-  std::unique_ptr<llvm::opt::InputArgList> Args(
-      Table.ParseArgs(&Argv[1], &Argv[Argc], MissingIndex, MissingCount));
-  if (MissingCount) {
-    std::string S;
-    llvm::raw_string_ostream OS(S);
-    OS << llvm::format("missing arg value for \"%s\", expected %d argument%s.",
-                       Args->getArgString(MissingIndex),
-                       MissingCount, (MissingCount == 1 ? "" : "s"));
-    OS.flush();
-    return lld::make_dynamic_error_code(StringRef(S));
-  }
-  for (auto *Arg : Args->filtered(OPT_UNKNOWN))
-    llvm::errs() << "ignoring unknown argument: " << Arg->getSpelling() << "\n";
-  return std::move(Args);
 }
 
 namespace lld {
