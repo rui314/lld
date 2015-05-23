1a1ee6dcddee5227298d67114d54182a38c8ff27
Rui Ueyama <ruiu@google.com>
2015-05-23 15:11:05 -0700

    Add Symbols.cpp.

--- COFF/InputFiles.cpp
+++ COFF/InputFiles.cpp
@@ -28,38 +28,6 @@ using llvm::sys::fs::file_magic;
 namespace lld {
 namespace coff {
 
-ErrorOr<std::unique_ptr<InputFile>> CanBeDefined::getMember() {
-  auto MBRefOrErr = File->getMember(&Sym);
-  if (auto EC = MBRefOrErr.getError())
-    return EC;
-  MemoryBufferRef MBRef = MBRefOrErr.get();
-
-  // getMember returns an empty buffer if the member was already
-  // read from the library.
-  if (MBRef.getBuffer().empty())
-    return nullptr;
-
-  file_magic Magic = identify_magic(MBRef.getBuffer());
-  if (Magic == file_magic::coff_import_library)
-    return llvm::make_unique<ImportFile>(MBRef);
-
-  if (Magic != file_magic::coff_object)
-    return make_dynamic_error_code("unknown file type");
-
-  StringRef Filename = MBRef.getBufferIdentifier();
-  ErrorOr<std::unique_ptr<ObjectFile>> FileOrErr = ObjectFile::create(Filename, MBRef);
-  if (auto EC = FileOrErr.getError())
-    return EC;
-  return std::move(FileOrErr.get());
-}
-
-bool Undefined::replaceWeakExternal() {
-  if (!WeakExternal || !*WeakExternal)
-    return false;
-  getSymbolRef()->Ptr = (*WeakExternal)->getSymbolRef()->Ptr;
-  return true;
-}
-
 static StringRef basename(StringRef Path) {
   size_t Pos = Path.rfind('\\');
   if (Pos == StringRef::npos)
